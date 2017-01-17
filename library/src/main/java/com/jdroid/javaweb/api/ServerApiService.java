ab6fce621d96d4848993000aafc19dc4853dab8c
Maxi Rosson <maxirosson@gmail.com>
2017-01-16 22:48:50 -0300

    Upgrade jdroid dependencies

--- library/src/main/java/com/jdroid/javaweb/api/ServerApiService.java
+++ library/src/main/java/com/jdroid/javaweb/api/ServerApiService.java
@@ -1,23 +1,17 @@
 package com.jdroid.javaweb.api;
 
-import com.jdroid.java.api.AbstractApiService;
+import com.jdroid.java.http.api.AbstractApiService;
 import com.jdroid.java.http.mock.AbstractMockHttpService;
 import com.jdroid.java.http.mock.JsonMockHttpService;
 import com.jdroid.javaweb.context.Application;
 
 public abstract class ServerApiService extends AbstractApiService {
 	
-	/**
-	 * @see com.jdroid.java.api.AbstractApiService#isHttpMockEnabled()
-	 */
 	@Override
 	protected Boolean isHttpMockEnabled() {
 		return Application.get().getAppContext().isHttpMockEnabled();
 	}
 	
-	/**
-	 * @see com.jdroid.java.api.AbstractApiService#getAbstractMockHttpServiceInstance(java.lang.Object[])
-	 */
 	@Override
 	protected AbstractMockHttpService getAbstractMockHttpServiceInstance(Object... urlSegments) {
 		return new JsonMockHttpService(urlSegments) {
