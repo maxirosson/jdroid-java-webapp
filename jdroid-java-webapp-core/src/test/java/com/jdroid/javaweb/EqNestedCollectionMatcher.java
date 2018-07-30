7c40d9883943b47b12c50bf1f1917373bccd40a6
Maxi Rosson <maxirosson@gmail.com>
2018-07-29 22:06:31 -0300

    Removed bean utils dependency from core lib

--- jdroid-java-webapp-core/src/test/java/com/jdroid/javaweb/EqNestedCollectionMatcher.java
+++ jdroid-java-webapp-core/src/test/java/com/jdroid/javaweb/EqNestedCollectionMatcher.java
@@ -1,7 +1,9 @@
 package com.jdroid.javaweb;
 
-import com.jdroid.javaweb.utils.ReflectionUtils;
+import com.jdroid.java.exception.UnexpectedException;
 
+import org.apache.commons.beanutils.NestedNullException;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.hamcrest.Matcher;
 import org.mockito.ArgumentMatcher;
 
@@ -34,6 +36,24 @@ public class EqNestedCollectionMatcher<T, S> extends ArgumentMatcher<T> {
 	 */
 	@Override
 	public boolean matches(Object argument) {
-		return matcher.matches(ReflectionUtils.getPropertyValue(argument, propertyName));
+		return matcher.matches(getPropertyValue(argument, propertyName));
+	}
+	
+	/**
+	 * @param <T> The type of the result object
+	 * @param from The object where invoke the property
+	 * @param propertyName The name of the property to invoke
+	 * @return The property value
+	 */
+	@SuppressWarnings("unchecked")
+	private static <T> T getPropertyValue(Object from, String propertyName) {
+		try {
+			return (T)PropertyUtils.getNestedProperty(from, propertyName);
+		} catch (NestedNullException exception) {
+			// if any the nested objects is null we return null as value
+			return null;
+		} catch (Exception ex) {
+			throw new UnexpectedException("Error getting the property: '" + propertyName + "' of: " + from);
+		}
 	}
 }
