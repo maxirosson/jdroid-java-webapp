c3eb6b46708e19efd5779eda5c4177a847310de7
Maxi Rosson <maxirosson@gmail.com>
2018-08-13 16:01:43 -0300

    Migrated devices repository to firestore

---
+++ jdroid-java-webapp-sample/src/main/java/com/jdroid/javaweb/sample/repository/DeviceFirestoreRepository.java
@@ -0,0 +1,44 @@
+package com.jdroid.javaweb.sample.repository;
+
+import com.google.cloud.firestore.Query;
+import com.jdroid.javaweb.firebase.firestore.FirestoreRepository;
+import com.jdroid.javaweb.push.Device;
+import com.jdroid.javaweb.push.DeviceRepository;
+import com.jdroid.javaweb.push.DeviceType;
+import com.jdroid.javaweb.sample.context.BuildConfig;
+import com.jdroid.javaweb.sample.context.ServerApplication;
+
+public class DeviceFirestoreRepository extends FirestoreRepository<Device> implements DeviceRepository {
+	
+	@Override
+	protected String getServiceAccountJsonPath() {
+		return BuildConfig.FIREBASE_SERVICE_ACCOUNT;
+	}
+	
+	@Override
+	protected String getProjectId() {
+		return ServerApplication.get().getFirebaseProjectId();
+	}
+	
+	@Override
+	protected String getPath() {
+		return "devices";
+	}
+	
+	@Override
+	protected Class<Device> getEntityClass() {
+		return Device.class;
+	}
+	
+	@Override
+	public Device findByRegistrationToken(String registrationToken, DeviceType deviceType) {
+		Query query = createCollectionReference().whereEqualTo("registrationToken", registrationToken).whereEqualTo("deviceType", deviceType);
+		return getItemByQuery(query);
+	}
+	
+	@Override
+	public Device findByInstanceId(String instanceId, DeviceType deviceType) {
+		Query query = createCollectionReference().whereEqualTo("instanceId", instanceId).whereEqualTo("deviceType", deviceType);
+		return getItemByQuery(query);
+	}
+}
