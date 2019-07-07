84773384711a2ebdf8ecdca7c049bc1f582680f0
Maxi Rosson <maxirosson@gmail.com>
2019-07-07 13:29:06 -0300

    Added AbstractScheduler class

---
+++ jdroid-java-webapp-core/src/main/java/com/jdroid/javaweb/scheduler/AbstractScheduler.kt
@@ -0,0 +1,59 @@
+package com.jdroid.javaweb.scheduler
+
+import com.jdroid.java.date.DateUtils
+import com.jdroid.java.utils.LoggerUtils
+
+import java.util.Date
+
+abstract class AbstractScheduler {
+
+    private var inProgress: Boolean = false
+    var lastExecutionStartDate: Date? = null
+        private set
+    var lastExecutionEndDate: Date? = null
+        private set
+
+    protected fun execute() {
+        if (isEnabled()) {
+            if (acquireLock()) {
+                try {
+                    doExecute()
+                } catch (e: Exception) {
+                    onException(e)
+                } finally {
+                    releaseLock()
+                }
+            } else {
+                LoggerUtils.getLogger(javaClass).info(javaClass.simpleName + " already in progress, skipping this schedule.")
+            }
+        } else {
+            LoggerUtils.getLogger(javaClass).info(javaClass.simpleName + " disabled, skipping this schedule.")
+        }
+    }
+
+    protected abstract fun doExecute()
+
+    protected open fun onException(e: Exception) {
+        LoggerUtils.getLogger(javaClass).error("Unexpected error when executing " + javaClass.simpleName, e)
+    }
+
+    protected open fun isEnabled(): Boolean {
+        return true
+    }
+
+    @Synchronized
+    private fun acquireLock(): Boolean {
+        if (!inProgress) {
+            inProgress = true
+            lastExecutionStartDate = DateUtils.now()
+            return true
+        }
+        return false
+    }
+
+    @Synchronized
+    private fun releaseLock() {
+        inProgress = false
+        lastExecutionEndDate = Date()
+    }
+}
