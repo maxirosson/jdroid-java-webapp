7c40d5c1c100e145d98c8af0f3017c5b2c506df4
Maxi Rosson <maxirosson@gmail.com>
2019-08-22 23:11:16 -0300

    Migrated dependencies to buildSrc

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,56 @@
+object Libs {
+    // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
+    const val jdroid_java_firebase_database = "com.jdroidtools:jdroid-java-firebase-database:3.0.0-SNAPSHOT"
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0-SNAPSHOT"
+    const val jdroid_java_remote_config = "com.jdroidtools:jdroid-java-remote-config:3.0.0-SNAPSHOT"
+
+    // https://github.com/maxirosson/jdroid-java-http/blob/master/CHANGELOG.md
+    const val jdroid_java_http = "com.jdroidtools:jdroid-java-http-core:3.0.0-SNAPSHOT"
+    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0-SNAPSHOT"
+
+    // https://github.com/getsentry/sentry-java
+    const val sentry = "com.getsentry.raven:raven:8.0.2"
+
+    // Logging
+    const val log4j = "log4j:log4j:1.2.17"
+    const val slf4j = "org.slf4j:slf4j-log4j12:1.7.25"
+
+    const val spring_core = "org.springframework:spring-core:3.2.18.RELEASE"
+    const val spring_web = "org.springframework:spring-web:3.2.18.RELEASE"
+    const val spring_webmvc = "org.springframework:spring-webmvc:3.2.18.RELEASE"
+    const val spring_context_support = "org.springframework:spring-context-support:3.2.18.RELEASE"
+
+    const val jackson_core = "com.fasterxml.jackson.core:jackson-core:2.8.7"
+    const val jackson_databind = "com.fasterxml.jackson.core:jackson-databind:2.8.7"
+    const val jackson_annotations = "com.fasterxml.jackson.core:jackson-annotations:2.8.7"
+
+    const val servlet_api = "javax.servlet:servlet-api:2.5"
+
+    const val json = "org.json:json:20131018"
+
+    // Open CSV
+    const val opencsv = "net.sf.opencsv:opencsv:2.3"
+
+    // Facebook
+    const val restfb = "com.restfb:restfb:1.6.12"
+
+    // https://firebase.google.com/support/release-notes/admin/java
+    const val firebase_admin = "com.google.firebase:firebase-admin:6.8.1"
+
+    const val guava = "com.google.guava:guava:20.0"
+    const val commons_beanutils = "commons-beanutils:commons-beanutils:1.9.3"
+
+    // Push for Android
+    const val json_simple = "com.googlecode.json-simple:json-simple:1.1"
+
+    // http://twitter4j.org/en/index.html#download
+    // https://github.com/Twitter4J/Twitter4J/releases
+    const val twitter4j_core = "org.twitter4j:twitter4j-core:4.0.7"
+    const val twitter4j_stream = "org.twitter4j:twitter4j-stream:4.0.7"
+
+    const val firebase_token_generator = "com.firebase:firebase-token-generator:2.0.0"
+
+    // Testing
+    const val testng = "org.testng:testng:6.8.21"
+    const val mockito = "org.mockito:mockito-all:1.10.19"
+}
