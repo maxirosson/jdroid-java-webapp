892221121459dd76b0ad9a65d54e0a9443a43f64
jdroid-ci <jdroidtools@gmail.com>
2019-09-01 01:52:21 +0000

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -9,16 +9,16 @@ object Libs {
     const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0-SNAPSHOT"
 
     // https://github.com/getsentry/sentry-java
-    const val sentry = "com.getsentry.raven:raven:8.0.2"
+    const val sentry = "com.getsentry.raven:raven:8.0.3"
 
     // Logging
     const val log4j = "log4j:log4j:1.2.17"
-    const val slf4j = "org.slf4j:slf4j-log4j12:1.7.25"
+    const val slf4j = "org.slf4j:slf4j-log4j12:1.7.28"
 
-    const val spring_core = "org.springframework:spring-core:3.2.18.RELEASE"
-    const val spring_web = "org.springframework:spring-web:3.2.18.RELEASE"
-    const val spring_webmvc = "org.springframework:spring-webmvc:3.2.18.RELEASE"
-    const val spring_context_support = "org.springframework:spring-context-support:3.2.18.RELEASE"
+    const val spring_core = "org.springframework:spring-core:5.1.9.RELEASE"
+    const val spring_web = "org.springframework:spring-web:5.1.9.RELEASE"
+    const val spring_webmvc = "org.springframework:spring-webmvc:5.1.9.RELEASE"
+    const val spring_context_support = "org.springframework:spring-context-support:5.1.9.RELEASE"
 
     const val jackson_core = "com.fasterxml.jackson.core:jackson-core:2.8.7"
     const val jackson_databind = "com.fasterxml.jackson.core:jackson-databind:2.8.7"
@@ -29,19 +29,19 @@ object Libs {
     const val json = "org.json:json:20131018"
 
     // Open CSV
-    const val opencsv = "net.sf.opencsv:opencsv:2.3"
+    const val opencsv = "net.sf.opencsv:opencsv:2.4"
 
     // Facebook
-    const val restfb = "com.restfb:restfb:1.6.12"
+    const val restfb = "com.restfb:restfb:2.24.1"
 
     // https://firebase.google.com/support/release-notes/admin/java
-    const val firebase_admin = "com.google.firebase:firebase-admin:6.8.1"
+    const val firebase_admin = "com.google.firebase:firebase-admin:6.9.0"
 
-    const val guava = "com.google.guava:guava:20.0"
-    const val commons_beanutils = "commons-beanutils:commons-beanutils:1.9.3"
+    const val guava = "com.google.guava:guava:23.0"
+    const val commons_beanutils = "commons-beanutils:commons-beanutils:1.9.4"
 
     // Push for Android
-    const val json_simple = "com.googlecode.json-simple:json-simple:1.1"
+    const val json_simple = "com.googlecode.json-simple:json-simple:1.1.1"
 
     // http://twitter4j.org/en/index.html#download
     // https://github.com/Twitter4J/Twitter4J/releases
@@ -51,6 +51,6 @@ object Libs {
     const val firebase_token_generator = "com.firebase:firebase-token-generator:2.0.0"
 
     // Testing
-    const val testng = "org.testng:testng:6.8.21"
+    const val testng = "org.testng:testng:7.0.0"
     const val mockito = "org.mockito:mockito-all:1.10.19"
 }
