07f5f06af231bac6d08ad4e1cfb96dcb4744e2ad
Maxi Rosson <maxirosson@gmail.com>
2019-10-26 20:47:14 -0300

    Libs constants to upper case

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,48 +1,48 @@
 object Libs {
     // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
-    const val jdroid_java_firebase_database = "com.jdroidtools:jdroid-java-firebase-database:3.0.0"
-    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
-    const val jdroid_java_remote_config = "com.jdroidtools:jdroid-java-remote-config:3.0.0"
+    const val JDROID_JAVA_FIREBASE_DATABASE = "com.jdroidtools:jdroid-java-firebase-database:3.0.0"
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.0.0"
+    const val JDROID_JAVA_REMOTE_CONFIG = "com.jdroidtools:jdroid-java-remote-config:3.0.0"
 
     // https://github.com/maxirosson/jdroid-java-http/blob/master/CHANGELOG.md
-    const val jdroid_java_http = "com.jdroidtools:jdroid-java-http-core:3.0.0"
-    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
+    const val JDROID_JAVA_HTTP = "com.jdroidtools:jdroid-java-http-core:3.0.0"
+    const val JDROID_JAVA_HTTP_OKHTTP = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
 
     // https://github.com/getsentry/sentry-java
-    const val sentry = "com.getsentry.raven:raven:8.0.3"
+    const val SENTRY = "com.getsentry.raven:raven:8.0.3"
 
     // Logging
-    const val log4j = "log4j:log4j:1.2.17"
-    const val slf4j = "org.slf4j:slf4j-log4j12:1.7.25"
+    const val LOGFJ = "log4j:log4j:1.2.17"
+    const val SLF4J = "org.slf4j:slf4j-log4j12:1.7.25"
 
-    const val spring_core = "org.springframework:spring-core:3.2.18.RELEASE"
-    const val spring_web = "org.springframework:spring-web:3.2.18.RELEASE"
-    const val spring_webmvc = "org.springframework:spring-webmvc:3.2.18.RELEASE"
-    const val spring_context_support = "org.springframework:spring-context-support:3.2.18.RELEASE"
+    const val SPRING_CORE = "org.springframework:spring-core:3.2.18.RELEASE"
+    const val SPRING_WEB = "org.springframework:spring-web:3.2.18.RELEASE"
+    const val SPRING_WEBMVC = "org.springframework:spring-webmvc:3.2.18.RELEASE"
+    const val SPRING_CONTEXT_SUPPORT = "org.springframework:spring-context-support:3.2.18.RELEASE"
 
-    const val jackson_core = "com.fasterxml.jackson.core:jackson-core:2.8.7"
-    const val jackson_databind = "com.fasterxml.jackson.core:jackson-databind:2.8.7"
-    const val jackson_annotations = "com.fasterxml.jackson.core:jackson-annotations:2.8.7"
+    const val JACKSON_CORE = "com.fasterxml.jackson.core:jackson-core:2.8.7"
+    const val JACKSON_DATABIND = "com.fasterxml.jackson.core:jackson-databind:2.8.7"
+    const val JACKSON_ANNOTATIONS = "com.fasterxml.jackson.core:jackson-annotations:2.8.7"
 
-    const val servlet_api = "javax.servlet:servlet-api:2.5"
+    const val SERVLET_API = "javax.servlet:servlet-api:2.5"
 
     // Open CSV
-    const val opencsv = "net.sf.opencsv:opencsv:2.3"
+    const val OPENCSV = "net.sf.opencsv:opencsv:2.3"
 
     // https://github.com/restfb/restfb
-    const val restfb = "com.restfb:restfb:1.6.12"
+    const val RESTFB = "com.restfb:restfb:1.6.12"
 
     // https://firebase.google.com/support/release-notes/admin/java
-    const val firebase_admin = "com.google.firebase:firebase-admin:6.8.1"
+    const val FIREBASE_ADMIN = "com.google.firebase:firebase-admin:6.8.1"
 
     // http://twitter4j.org/en/index.html#download
     // https://github.com/Twitter4J/Twitter4J/releases
-    const val twitter4j_core = "org.twitter4j:twitter4j-core:4.0.7"
-    const val twitter4j_stream = "org.twitter4j:twitter4j-stream:4.0.7"
+    const val TWITTER4J_CORE = "org.twitter4j:twitter4j-core:4.0.7"
+    const val TWITTER4J_STREAM = "org.twitter4j:twitter4j-stream:4.0.7"
 
-    const val firebase_token_generator = "com.firebase:firebase-token-generator:2.0.0"
+    const val FIREBASE_TOKEN_GENERATOR = "com.firebase:firebase-token-generator:2.0.0"
 
     // Testing
-    const val junit = "junit:junit:4.12"
-    const val mockito = "org.mockito:mockito-all:1.10.19"
+    const val JUNIT = "junit:junit:4.12"
+    const val MOCKITO = "org.mockito:mockito-all:1.10.19"
 }
