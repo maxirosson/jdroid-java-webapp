89a6b19e2abde9d9af07863b991f18f04e2aae07
Maxi Rosson <maxirosson@gmail.com>
2020-09-11 13:56:09 -0300

    Upgrade to Firebase Admin 7.0.0

--- jdroid-java-webapp-firebase-admin/src/main/java/com/jdroid/javaweb/firebase/admin/FirebaseAdminSdkHelper.kt
+++ jdroid-java-webapp-firebase-admin/src/main/java/com/jdroid/javaweb/firebase/admin/FirebaseAdminSdkHelper.kt
@@ -3,32 +3,23 @@ package com.jdroid.javaweb.firebase.admin
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import com.jdroid.java.exception.UnexpectedException
 import java.io.FileInputStream
-import java.io.IOException
+import java.io.InputStream
 
 object FirebaseAdminSdkHelper {
 
     fun init(serviceAccountJsonPath: String) {
-        try {
-            val serviceAccountStream = FileInputStream(serviceAccountJsonPath)
-            val builder = FirebaseOptions.Builder()
-            builder.setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
-            val options = builder.build()
-            FirebaseApp.initializeApp(options)
-        } catch (e: IOException) {
-            throw UnexpectedException(e)
-        }
+        init(FileInputStream(serviceAccountJsonPath))
     }
 
     fun initWithServiceAccount(serviceAccount: String) {
-        try {
-            val builder = FirebaseOptions.Builder()
-            builder.setCredentials(GoogleCredentials.fromStream(serviceAccount.byteInputStream()))
-            val options = builder.build()
-            FirebaseApp.initializeApp(options)
-        } catch (e: IOException) {
-            throw UnexpectedException(e)
-        }
+        init(serviceAccount.byteInputStream())
+    }
+
+    private fun init(serviceAccountStream: InputStream) {
+        val builder = FirebaseOptions.builder()
+        builder.setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+        val options = builder.build()
+        FirebaseApp.initializeApp(options)
     }
 }
